7ebb09e61cb48bb5cd7ef91586987e584ec530e2
siddhantsaigaonkar <siddhantsaigaonkar14k@gmail.com>
2024-03-04 22:19:31 +0530

    cal project

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+let btns = document.querySelectorAll(".btns");
+let buttonarray = Array.from(btns);
+
+let string = "";
+buttonarray.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerText === "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    } else if (e.target.innerHTML == "Ac") {
+      string = "";
+      display.value = string;
+    } else if (e.target.innerText === "=") {
+      string = eval(string);
+      display.value = string;
+    } else {
+      string += e.target.innerText;
+      display.value = string;
+    }
+  });
+});
